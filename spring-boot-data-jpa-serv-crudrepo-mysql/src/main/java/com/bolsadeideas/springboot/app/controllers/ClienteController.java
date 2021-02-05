99ff77c74df30f4ecd32a19b04e846383ebc53c4
Adrian Medina <medinalarry96@gmail.com>
2021-02-05 19:33:07 +0100

    configurando mysql al progetto crudRepo

---
+++ spring-boot-data-jpa-serv-crudrepo-mysql/src/main/java/com/bolsadeideas/springboot/app/controllers/ClienteController.java
@@ -0,0 +1,92 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+import com.bolsadeideas.springboot.app.models.service.IClienteCrudRepoService;
+//import com.bolsadeideas.springboot.app.models.service.IClienteService; // vecchio servizio applicativo
+
+@Controller
+@SessionAttributes("cliente")
+public class ClienteController {
+
+	@Autowired
+//	@Qualifier("clienteDaoJPA")
+	//private IClienteService clienteService; // Se innietta il servizio applicativo DAO
+	private IClienteCrudRepoService clienteService; // Se innietta il servizio applicativo di CrudRepository<T,ID>
+	
+	
+	@RequestMapping(value="/listar", method=RequestMethod.GET)
+	public String listar(Model model) {
+		model.addAttribute("titulo", "Lista dei clienti CrudRepository");
+		model.addAttribute("clientes", clienteService.findAll());
+		return "listar";
+	}
+	
+	@RequestMapping(value="/form")
+	public String crear(Map<String, Object> model) {
+		Cliente cliente = new Cliente();
+		model.put("cliente", cliente);
+		model.put("titulo", "Modulo Cliente");
+		return "form";
+	}
+	
+	@RequestMapping(value="/form/{id}")
+	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model) {
+		
+		Cliente cliente = null;
+		
+		if(id>0) {
+			cliente = clienteService.findOne(id);
+		} else {
+			return "redirect:/listar";
+		}
+
+		model.put("cliente", cliente);
+		model.put("titulo", "Editar Cliente");
+		
+		return "form";
+	}
+
+	@RequestMapping(value="/eliminar/{id}")
+	public String eliminar(@PathVariable(value="id") Long id) {
+				
+		if(id>0) {
+			clienteService.delete(id);
+		}		
+		return "redirect:/listar";
+	}
+
+	/*
+	 * CLiente e BindingResult vanno insieme.
+	 * BindingResult possiede i metod per controllare gli errori.
+	 * Il cliente si passa automaticamente se ha lo stesso nome del
+	 * model.put("cliente",..) del metodo crear(Map<string, Obj> model) @RequestMapping(value="/form")
+	 * */
+	@RequestMapping(value="/form", method=RequestMethod.POST)
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+		
+		if(result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario del Ciente");
+			return "form";
+		}
+		
+		clienteService.save(cliente);
+		status.setComplete();
+		return "redirect:listar";
+	}
+	
+}
---
+++ spring-boot-data-jpa-serv-crudrepo-mysql/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Adrian', 'Rodriguez', 'adrian14@email.com', '2020-03-04');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Tommaso', 'Paradiso', 'paradiso@email.com', '2019-09-17');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(3, 'Luca', 'Carboni', 'carboni@email.com', '2016-03-04');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(4, 'Alejandra', 'Rodriguez', 'malej8@email.com', '2008-08-05');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(5, 'Ernesto', 'Fantilli', 'erni@email.com', '2006-09-08');
